e1c00089c18e5360f991f363acbce790e14b042c
aishangwei <42730783+aishangwei@users.noreply.github.com>
2018-11-25 10:55:23 +0800

    Update pre_admin_node.sh

--- ceph-deploy/pre_admin_node.sh
+++ ceph-deploy/pre_admin_node.sh
@@ -17,7 +17,7 @@ mkdir  my-cluster
 cd my-cluster
 
 # 创建节点1
-ceph-deploy  new  c720181
+ceph-deploy  new  c720181 c720182 c720183
 
 ls
 # 编辑 ceph.conf 配置文件
@@ -30,9 +30,40 @@ cluster network = 192.168.20.0/24
 # 安装 ceph包，替代  ceph-deploy install  node1  node2 ,不过下面的命令需要在每台node上安装
 yum install -y ceph ceph-radosgw
 
+
 # 配置初始 monitor(s)、并收集所有密钥：
 ceph-deploy mon create-initial
 ls -l *.keyring
+
+# 把配置信息拷贝到各节点
+ceph-deploy admin c720181 c720182 c720183
+
+# 配置 osd
+for dev in "/dev/vda /dev/vdc /dev/vdd"
+do 
+  ceph-deploy disk zap c720181 $dev
+  ceph-deploy osd create c720181 --data $dev
+  ceph-deploy disk zap c720182 $dev
+  ceph-deploy osd create c720182 --data $dev
+  ceph-deploy disk zap c720183 $dev
+  ceph-deploy osd create c720183 --data $dev
+done
+
+# 部署 mgr
+ceph-deploy mgr create c720181 c720182 c720183
+
+# 开启 dashboard 模块，用于UI查看
+ceph mgr module enable dashboard
+
+
+
+
+
+
+
+
+
+
 
 
 
